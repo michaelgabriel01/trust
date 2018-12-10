b85d973668c4aa014ba210490ecb5f165c4c6782
MichaelJames02 <michael4achieve@gmail.com>
2018-12-10 11:58:25 -0200

    Updated views.py

--- users/views.py
+++ users/views.py
@@ -4,10 +4,13 @@ from django.views import generic
 
 from .forms import UserCreationForm
 
+
+# Register view
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
+# Our service view
 def our_service(request):
     return render(request, 'our-service.html')
